4920da117d7f47c016621f8bc06cb9266196ad56
unknown <tu@correo.com>
2026-04-27 10:21:30 -0500

    debug: mostrar valor de q en pantalla

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import HomeClient from './HomeClient'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Page({ searchParams }) {
   const { q = '' } = await searchParams
   return <HomeClient q={q} />
